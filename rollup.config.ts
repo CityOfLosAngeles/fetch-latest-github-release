6a8412772b1e43e8b945cfb4b4043e79242cfe04
jaylenw <jaylen.wimbish@lacity.org>
2025-06-30 16:58:17 +0000

    update rollup config and dist close #57

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,7 +12,18 @@ const config = {
     format: 'es',
     sourcemap: true
   },
-  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
+  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
+  external: [
+    '@actions/core',
+    '@actions/github',
+    'universal-user-agent',
+    '@octokit/core',
+    '@octokit/request',
+    '@octokit/graphql',
+    '@octokit/endpoint',
+    'config',
+    'semver'
+  ]
 }
 
 export default config
